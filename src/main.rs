89da24fe4806f20f0c35ae736aeb548b8e8c6e80
Eugene Tolbakov <ev.tolbakov@gmail.com>
2023-11-16 11:04:41 +0000

    chore: apply formatting to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod git;
 
-use std::{path::Path, io, str::FromStr};
-use clap::{Parser, arg, command};
-use git2::{Repository, Oid};
+use crate::git::{
+    checkout_last_commit, get_branch_name, get_commit_ids, move_git_pointer, repo, show_commit_info,
+};
+use clap::{arg, command, Parser};
 use cli_table::{format::Justify, Cell, Table};
-use crate::git::{repo, get_commit_ids, move_git_pointer, checkout_last_commit, get_branch_name, show_commit_info};
+use git2::{Oid, Repository};
+use std::{io, path::Path, str::FromStr};
 
 #[derive(Parser)]
 #[command(name = "Archeologit")]
@@ -27,19 +29,18 @@ enum Cmd {
 }
 
 impl FromStr for Cmd {
-
     type Err = ();
 
     fn from_str(input: &str) -> Result<Cmd, Self::Err> {
         let uppercased_input = input.trim().to_uppercase();
         let mut args = uppercased_input.split_whitespace();
         match args.next().unwrap() {
-            "B"     => Ok(Cmd::Back(get_step_size(args.next()))),
-            "N"     => Ok(Cmd::Next(get_step_size(args.next()))),
-            "I"     => Ok(Cmd::Info),
-            "Q"     => Ok(Cmd::Quit),
-            "H"     => Ok(Cmd::Help),
-            _       => Ok(Cmd::Noop),
+            "B" => Ok(Cmd::Back(get_step_size(args.next()))),
+            "N" => Ok(Cmd::Next(get_step_size(args.next()))),
+            "I" => Ok(Cmd::Info),
+            "Q" => Ok(Cmd::Quit),
+            "H" => Ok(Cmd::Help),
+            _ => Ok(Cmd::Noop),
         }
     }
 }
@@ -48,7 +49,7 @@ fn get_progress(current: usize, total: usize) -> String {
     format!("[{}/{}]", current + 1, total + 1)
 }
 
-fn get_step_size(maybe_step : Option<&str>) -> usize {
+fn get_step_size(maybe_step: Option<&str>) -> usize {
     match maybe_step {
         Some(s) => s.parse().unwrap_or(DEFAULT_STEP),
         None => 1,
@@ -56,7 +57,7 @@ fn get_step_size(maybe_step : Option<&str>) -> usize {
 }
 
 static DEFAULT_STEP: usize = 1;
-static QUIT_MESSAGE:&str = "Moving to the latest commit and quiting!";
+static QUIT_MESSAGE: &str = "Moving to the latest commit and quiting!";
 static HELP_MESSAGE: &str = "Archeologit v1.0.0. Reference
 Supported commands are case insensitive\n\
 ";
@@ -67,22 +68,47 @@ fn main() {
     let repo: Repository = repo(Path::new(&cli.path));
     let commit_ids: &[Oid] = &match get_commit_ids(&repo) {
         Ok(ids) => ids,
-        Err(e) => panic!("failed to get commits, {:?}", e)
+        Err(e) => panic!("failed to get commits, {:?}", e),
     };
-    
+
     let mut index = 0;
     let max_index = commit_ids.len() - 1;
     let branch_name = get_branch_name(&repo).unwrap();
-    println!("Total number of commits = {} on a branch '{}'", commit_ids.len(), branch_name);
+    println!(
+        "Total number of commits = {} on a branch '{}'",
+        commit_ids.len(),
+        branch_name
+    );
     move_git_pointer(&repo, commit_ids[index], get_progress(index, max_index));
 
     let help_table = vec![
-        vec!["b <step>".cell(), "⏭  go to previous <step> commits; <step> is 1 by default".cell().justify(Justify::Left)],
-        vec!["n <step>".cell(), "⏮  go to next <step> commits; <step> is 1 by default".cell().justify(Justify::Left)],
-        vec!["i".cell(), "🔎 get information about current commit: file path and status".cell().justify(Justify::Left)],
-        vec!["h".cell(), "🤓 help, show command options".cell().justify(Justify::Left)],
-        vec!["q".cell(), "👋 quit".cell().justify(Justify::Left)],                
-    ].table();
+        vec![
+            "b <step>".cell(),
+            "⏭  go to previous <step> commits; <step> is 1 by default"
+                .cell()
+                .justify(Justify::Left),
+        ],
+        vec![
+            "n <step>".cell(),
+            "⏮  go to next <step> commits; <step> is 1 by default"
+                .cell()
+                .justify(Justify::Left),
+        ],
+        vec![
+            "i".cell(),
+            "🔎 get information about current commit: file path and status"
+                .cell()
+                .justify(Justify::Left),
+        ],
+        vec![
+            "h".cell(),
+            "🤓 help, show command options"
+                .cell()
+                .justify(Justify::Left),
+        ],
+        vec!["q".cell(), "👋 quit".cell().justify(Justify::Left)],
+    ]
+    .table();
 
     loop {
         let mut command = String::new();
@@ -103,18 +129,18 @@ fn main() {
                 index += n;
                 if index >= max_index {
                     index = max_index;
-                }  
-                print!("⏮ ");                          
+                }
+                print!("⏮ ");
                 move_git_pointer(&repo, commit_ids[index], get_progress(index, max_index));
-            },
-            Cmd::Info => show_commit_info(&repo, commit_ids[index]),            
+            }
+            Cmd::Info => show_commit_info(&repo, commit_ids[index]),
             Cmd::Quit => {
-                println!("👋 {}", QUIT_MESSAGE);            
+                println!("👋 {}", QUIT_MESSAGE);
                 checkout_last_commit(&repo, &branch_name).unwrap();
                 break;
-            },
+            }
             Cmd::Help => println!("🤓 {}\n{}", HELP_MESSAGE, help_table.display().unwrap()),
             Cmd::Noop => println!("🙈 The command '{}' is not supported", &command.trim()),
         }
-    }    
+    }
 }
